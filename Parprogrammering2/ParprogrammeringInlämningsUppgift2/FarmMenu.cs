41f188b52e0f16af49d5cab67b849be43a1683f5
KalistheG <144893240+KalistheG@users.noreply.github.com>
2023-10-19 09:56:22 +0200

    Update FarmMenu.cs

--- Parprogrammering2/ParprogrammeringInlämningsUppgift2/FarmMenu.cs
+++ Parprogrammering2/ParprogrammeringInlämningsUppgift2/FarmMenu.cs
@@ -8,7 +8,64 @@ namespace ParprogrammeringInlämningsUppgift2
 {
     internal class FarmMenu
     {
+        private AnimalManager animalManger;
+        private Cropmanager cropManager;
 
+       public FarmMneu()
+        {
+            animalManager = new AnimalManager();
+            cropManager = new CropManager();
+        }
+        public void Handelcropmanagement()
+        {
+            Console.WriteLine(" Crop Management menu:");
+            Console.WriteLine("1. View Crops");
+            Console.WriteLine("2. Manage Crops");
+
+            int choice;
+            if (int.TryParce(Console.ReadLine(), out choice))
+            {
+                swich (choice)
+                {
+                    case 1: 
+                      cropManager.ViewCrops();
+                        break:
+                    case 2:
+                    Console.WriteLine("Enter Crop ID:");
+                    int cropId = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Crop Name:");
+                    string cropName = Console.ReadLine();
+                    Console.WriteLine("Enter Crop Type:");
+                    string cropType = Console.ReadLine();
+                    Console.WriteLine("Enter Quantity:");
+                    int cropQuantity = int.Parse(Console.ReadLine());
+
+                Crop newCrop = new Crop(cropId, cropName, cropType, cropQuantity);
+                cropManager.AddCrop(newCrop);
+
+                Console.WriteLine("Crop added successfully.");
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+            
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number");
+            }
+        
+          
+            
+
+
+
+
+
+
+
+        
         public void MainMenu()
         {
             Console.WriteLine("Hej välkommen till vår bondgård!");
