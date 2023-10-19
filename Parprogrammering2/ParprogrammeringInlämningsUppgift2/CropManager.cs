16f21597ab1b35653f98cd3daaf778d798aa50c7
KalistheG <144893240+KalistheG@users.noreply.github.com>
2023-10-19 09:00:37 +0200

    Update CropManager.cs

--- Parprogrammering2/ParprogrammeringInlämningsUppgift2/CropManager.cs
+++ Parprogrammering2/ParprogrammeringInlämningsUppgift2/CropManager.cs
@@ -8,6 +8,25 @@ namespace ParprogrammeringInlämningsUppgift2
 {
     internal class CropManager
     {
+      Crop wheat = new Crop(1, "Wheat Field", "wheat", 100);
+      Crop corn = new Crop(2, "Corn Field", "corn", 75);
+
+    List<Crop> CropList = new List<Crop>();
+
+    Public CropManager ()
+    {
+      CropList.Add(Wheat);
+      CropList.Add(Corn);
+    }
+    Public void Viewcrops()
+     {
+        Console.WriteLine("Crops on the farm") //kan ändra text bara ett exempel
+        Foreach (Crop crop in Croplist) 
+        {
+            crop.GetDiscribtion();
+        }
+     }
+        
      
     }
 }
