5923d3e0a9465482a651e8ee40aa223fc8d32d50
KalistheG <144893240+KalistheG@users.noreply.github.com>
2023-10-19 10:13:23 +0200

    Update FarmMenu.cs

--- Parprogrammering2/ParprogrammeringInlämningsUppgift2/FarmMenu.cs
+++ Parprogrammering2/ParprogrammeringInlämningsUppgift2/FarmMenu.cs
@@ -54,27 +54,33 @@ namespace ParprogrammeringInlämningsUppgift2
             else
             {
                 Console.WriteLine("Invalid input. Please enter a number");
-            }
-        
-          
-            
+            }  
 
-
-
-
-
-
-
-        
         public void MainMenu()
         {
             Console.WriteLine("Hej välkommen till vår bondgård!");
-            Console.WriteLine("Vänligen välj vad du vill göra.");
-            Console.WriteLine("1: Hantera djuren.");
-            Console.WriteLine("2: Hantera skörden");
+
+            while (true)
+            {
+                Console.WriteLine("Vänligen välj vad du vill göra.");
+                Console.WriteLine("1: Hantera djuren.");
+                Console.WriteLine("2: Hantera skörden");
+            }
+
+            int choice
+              if (int.TryParce(Console.ReadLine(), out choice))
+            {
+                swich (choice)
+                {
+                    case 1: 
+                      HandelAnimalMangement();
+                    break:
+                     case 2. HandelCropMangement();
+                    break:
+                         
 
             AnimalManager animalManager = new AnimalManager();
-            animalManager.ViewAnimal();
+          
 
 
 
